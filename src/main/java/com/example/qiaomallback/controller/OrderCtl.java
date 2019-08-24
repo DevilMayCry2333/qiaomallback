b0eaea3869ede441b6da122eb80aeb5063c91c97
DevilMayCry2333 <1712363499@qq.com>
2019-08-24 14:17:39 +0800

    最新的

--- src/main/java/com/example/qiaomallback/controller/OrderCtl.java
+++ src/main/java/com/example/qiaomallback/controller/OrderCtl.java
@@ -1,10 +1,12 @@
 package com.example.qiaomallback.controller;
 
 import com.example.qiaomallback.service.OrderSer;
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +23,27 @@ public class OrderCtl {
         jsonObject.put("orderData",orderSer.selectAllOrder());
         return jsonObject;
 //        return orderSer.selectAllOrder();
+    }
+
+    @RequestMapping("/Order/OrderBasicDetail")
+    public Object OrderBasicDetail(@RequestParam String id){
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.add(orderSer.OrderBasicDetail(id));
+        return jsonArray;
+    }
+
+    @RequestMapping("/Order/OrderItemById")
+    public Object OrderItemById(@RequestParam String id){
+        return orderSer.orderItemById(Long.valueOf(id));
+    }
+
+    @RequestMapping("/delOrder")
+    public Object DelOrder(@RequestParam String id){
+        JSONObject jsonObject = new JSONObject();
+
+        orderSer.delOrder(Long.valueOf(id));
+        jsonObject.put("Res",true);
+        return jsonObject;
+
     }
 }
